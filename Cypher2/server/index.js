582329825603d91849170c543fe6d2dd8eb2e500
leonfullxr <l.elliottfuller@gmail.com>
2024-12-31 16:22:10 +0100

    Backed from Cypher2 completed

---
+++ Cypher2/server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const coors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/connectDB');
+const router = require('./routes/index');
+const cookiesParser = require('cookie-parser');
+
+const app = express();
+app.use(coors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookiesParser());
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.json({ 
+        message: 'Server is running at ' + PORT 
+})});
+
+// api endpoints
+app.use('/api', router);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
