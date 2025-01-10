8451563cf9baf7faa2ffbe6d7414b68078ecf8a6
leonfullxr <l.elliottfuller@gmail.com>
2025-01-10 17:38:58 +0100

    added socket

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,36 +1,40 @@
 import React, { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { logout, setUser } from "../redux/userSlice";
 import SideBar from "../components/SideBar";
+import logo from "../assets/logo2.png";
+import io from "socket.io-client";
+import { setOnlineUser } from "../redux/userSlice";
 
 const Home = () => {
-    const user = useSelector((state) => state.user);
+    const user = useSelector(state => state.user);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const location = useLocation();
 
-    console.log("redux user", user);
+    console.log("user", user);
 
     const fetchUserDetails = async () => {
         try {
             const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`;
             const response = await axios({
-                method :'GET',
+                //method :'GET',
                 url : URL,
                 withCredentials : true
             })
 
-            dispatch(setUser(response.data));
+            dispatch(setUser(response.data.data));
 
-            if(response.data.logout){
+            if(response.data.data.logout){
                 dispatch(logout());
                 navigate('/email');
             }
             console.log("current user details", response);
         } catch (error) {
-            console.log(error);
+            console.log("error", error);
         }
     }
 
@@ -38,18 +42,50 @@ const Home = () => {
         fetchUserDetails();
     },[]);
 
-    return (
-        <div className="grid lg:grid.cols-[310px,1fr] h-screen max-h-screen">
-            <section className="bg-white">
-                <SideBar/>
-            </section>
+    /** socket connection **/
+    useEffect(() => {
+        const socketConnection = io(process.env.REACT_APP_BACKEND_URL, {
+            auth : {
+                token : localStorage.getItem('token')
+            }
+        })
 
-        {/* Main content */}
-            <section>
-                <Outlet/>
-            </section>
+        socketConnection.on('online-user', (data) => {
+            console.log('online-user', data);
+            dispatch(setOnlineUser(data));
+        })
+
+        return () => {
+            socketConnection.disconnect();
+        }
+    },[]);
+
+
+    const basePath = location.pathname === '/'
+  return (
+    <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
+        <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
+           <SideBar/>
+        </section>
+
+        {/**message component**/}
+        <section className={`${basePath && "hidden"}`} >
+            <Outlet/>
+        </section>
+
+
+        <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex" }`}>
+            <div>
+              <img
+                src={logo}
+                width={250}
+                alt='logo'
+              />
+            </div>
+            <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
         </div>
-    );
+    </div>
+  )
 }
 
 export default Home;
