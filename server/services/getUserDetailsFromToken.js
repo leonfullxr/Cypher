d0919c1fe3b343f7e208af74ebae1a75105d0c87
leonfullxr <l.elliottfuller@gmail.com>
2025-01-10 17:15:32 +0100

    Handled expired tokens

--- server/services/getUserDetailsFromToken.js
+++ server/services/getUserDetailsFromToken.js
@@ -2,18 +2,32 @@ const jwt = require('jsonwebtoken');
 const UserModel = require('../models/UserModel');
 
 const getUserDetailsFromToken = async (token) => {
-    if(!token){
+    if (!token) {
         return {
-            message : 'No token provided',
-            logout: true
-        }
+            message: 'No token provided',
+            logout: true,
+        };
     }
 
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+    try {
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
 
-    const user = await UserModel.findById(decoded.id).select('-password');
+        const user = await UserModel.findById(decoded.id).select('-password');
 
-    return user;
-}
+        return user;
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return {
+                message: 'Token has expired',
+                logout: true,
+            };
+        }
+
+        return {
+            message: 'Invalid token',
+            logout: true,
+        };
+    }
+};
 
 module.exports = getUserDetailsFromToken;
