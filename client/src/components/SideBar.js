b6e879107ced0bacdb13d34f783e8e956cc09ec2
leonfullxr <l.elliottfuller@gmail.com>
2025-01-10 17:38:37 +0100

    added search icon, logout, and user

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -6,13 +6,17 @@ import { BiLogOut } from "react-icons/bi";
 import Avatar from "./Avatar";
 import { useSelector } from "react-redux";
 import EditUserDetails from "./EditUserDetails";
+import { FiArrowUpLeft } from "react-icons/fi";
+import SearchUser from "./SearchUser";
 
 const SideBar = () => {
     const user = useSelector((state) => state?.user);
     const [editUserOpen, setEditUserOpen] = useState(false);
+    const [allUsers, setAllUsers] = useState([]);
+    const [openSearchUser, setOpenSearchUser] = useState(false);
 
     return (
-        <div className="w-full h-full">
+        <div className="w-full h-full grid grid-cols-[48px,1fr]">
             <div className='bg-slate-100 w-12 h-full rounded-tr-lg rounded-br-lg py-5 text-slate-600 flex flex-col justify-between'>
                     <div>
                         <NavLink className={({isActive})=>`w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded ${isActive && "bg-slate-200"}`} title='chat'>
@@ -21,7 +25,7 @@ const SideBar = () => {
                         />
                         </NavLink>
 
-                        <div title='add friend' className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded' >
+                        <div title='add friend' onClick={setOpenSearchUser} className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded' >
                             <FaUserPlus size={25}/>
                         </div>
                     </div>
@@ -42,10 +46,40 @@ const SideBar = () => {
                     </button>
                     </div>
             </div>
+
+            <div className="w-full">
+                <div className="flex items-center h-16">
+                    <h2 className="text-xl font-bold p-2 text-slate-800 h-16">Message</h2>
+                </div>
+                <div className="bg-slate-200 p-[0.5px]"> </div>
+                
+                <div className="h-[calc(100vh-60px)] overflow-x-hidden overflow-y-auto scrollbar">
+                {
+                        allUsers.length === 0 && (
+                            <div className='mt-12'>
+                                <div className='flex justify-center items-center my-4 text-slate-500'>
+                                    <FiArrowUpLeft
+                                        size={50}
+                                    />
+                                </div>
+                                <p className='text-lg text-center text-slate-400'>Explore users to start a conversation with.</p>    
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
             {/* Edit user details */}
             {editUserOpen && (
                 <EditUserDetails onClose={()=>setEditUserOpen(false)} user={user}/>
             )}
+
+            {/* Add friend */}
+            {
+                openSearchUser && (
+                    <SearchUser onClose={()=>setOpenSearchUser(false)}/>
+                )
+            }
+
         </div>
 
     );
