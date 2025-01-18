527ddfa1d73a4245e141398856206ec93bf56f0f
leonfullxr <l.elliottfuller@gmail.com>
2025-01-18 12:30:42 +0100

    refactor code

--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -2,19 +2,20 @@ import React from "react";
 import logo from "../assets/logo2.png";
 
 const AuthLayouts = ({children}) => {
-    return (
+  return (
     <>
-        <header className="flex justify-center items-center py-4 h-30 shadow-md bg-white">
-            <img 
-            src={logo} 
-            alt="logo"
+      <header className='flex justify-center items-center py-3 h-20 shadow-md bg-white'>
+          <img 
+            src={logo}
+            alt='logo'
             width={180}
-            height={60} 
-            />
-        </header>
-        { children }
-    </>
-    );
-}
+            height={60}
+          />
+      </header>
 
-export default AuthLayouts;
+      { children }
+    </>
+  )
+}
+  
+export default AuthLayouts
