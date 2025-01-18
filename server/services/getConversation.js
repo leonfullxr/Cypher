88d2c3ad79b890022c4fc84ed13d7a9ded20d924
leonfullxr <l.elliottfuller@gmail.com>
2025-01-18 13:27:24 +0100

    restructured getconversation

---
+++ server/services/getConversation.js
@@ -0,0 +1,29 @@
+const { ConversationModel } = require('../models/ConversationModel.js')
+
+const getConversation = async (currentUserId) => {
+    if(currentUserId) {
+        const currentUserConversation = await ConversationModel.find({
+            $or : [
+                { sender : currentUserId },
+                { receiver : currentUserId }
+            ]
+        }).sort({ updatedAt : -1 }).populate('message').populate('sender').populate('receiver')
+
+        const conversation = currentUserConversation.map((conv)=>{
+            const countUnseenMsg = conv?.message.reduce((preve,curr) => preve + (curr.seen ? 0 : 1),0)
+            return {
+                _id : conv?._id,
+                sender : conv?.sender,
+                receiver : conv?.receiver,
+                unseenMessage : countUnseenMsg,
+                lastMsg : conv?.message[conv?.message.length - 1]
+            }
+        })
+
+        return conversation
+    } else {
+        return []
+    }
+}
+
+module.exports = getConversation;
