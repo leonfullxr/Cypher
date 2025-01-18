40ca18bc7a5841e8631268fc909c106fcf0299b6
leonfullxr <l.elliottfuller@gmail.com>
2025-01-18 14:03:56 +0100

    fixed string comparations

--- server/services/getConversation.js
+++ server/services/getConversation.js
@@ -10,13 +10,21 @@ const getConversation = async (currentUserId) => {
         }).sort({ updatedAt : -1 }).populate('message').populate('sender').populate('receiver')
 
         const conversation = currentUserConversation.map((conv)=>{
-            const countUnseenMsg = conv?.message.reduce((preve,curr) => preve + (curr.seen ? 0 : 1),0)
+            const countUnseenMsg = conv?.message?.reduce((preve,curr) => {
+                const msgByUserId = curr?.msgByUserId?.toString()
+                if(msgByUserId !== currentUserId) {
+                    return preve + (curr.seen ? 0 : 1)
+                } else {
+                    return preve
+                }
+                
+            },0)
             return {
                 _id : conv?._id,
                 sender : conv?.sender,
                 receiver : conv?.receiver,
                 unseenMessage : countUnseenMsg,
-                lastMsg : conv?.message[conv?.message.length - 1]
+                lastMsg : conv?.message[conv?.message?.length - 1]
             }
         })
 
