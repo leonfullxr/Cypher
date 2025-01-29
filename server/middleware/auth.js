ede0194dee489d932e4d3c8296709958c3b5f183
marianoaguilar <aguilarolmomariano@gmail.com>
2025-01-29 18:15:27 +0100

    updated comments

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,26 +1,25 @@
-// middleware/auth.js
 const jwt = require('jsonwebtoken');
 const UserModel = require('../models/UserModel');
 
 async function authenticateJWT(req, res, next) {
   try {
-    // 1. Tomar el token de la cookie
+    // 1. Get the token from the cookie
     const token = req.cookies.token;
 
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized. No token provided."});
+      return res.status(401).json({ message: "Unauthorized. No token provided." });
     }
 
-    // 2. Verificar el token con tu SECRET
+    // 2. Verify the token using your SECRET
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // 3. Buscar al usuario en la DB a partir de decoded.id (o lo que guardes en tokenData)
+    // 3. Find the user in the database using decoded.id
     const user = await UserModel.findById(decoded.id);
     if (!user) {
-      return res.status(401).json({ message: "Unauthorized. User not found."});
+      return res.status(401).json({ message: "Unauthorized. User not found." });
     }
 
-    // 4. Guardar info en req.user y pasar al siguiente middleware
+    // 4. Store user info in req.user and proceed to the next middleware
     req.user = user;
     next();
 
