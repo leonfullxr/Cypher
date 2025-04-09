d335f22bd02fa2cea03b8e515a644270a0396b6c
leonfullxr <l.elliottfuller@gmail.com>
2025-04-09 23:16:05 +0200

    added image to forgot page

--- client/src/pages/ForgotPasswordPage.js
+++ client/src/pages/ForgotPasswordPage.js
@@ -3,9 +3,13 @@ import React from "react";
 const ForgotPassword = () => {
     return (
         <div>
-            <h1>Forgot Password</h1>
+            <img 
+                src="https://res.cloudinary.com/du0ukbkvu/image/upload/v1744233141/Cypher-file/onm1cfxumymesusjfxjq.png" 
+                alt="Forgot Password Meme" 
+                style={{ maxWidth: "50%", height: "auto", marginTop: "1rem" }} 
+            />
         </div>
-    )
+    );
 }
 
 export default ForgotPassword;
