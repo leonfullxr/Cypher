453e1c17c4d83fd7074d0341610d1ec2c6dd66be
marianoaguilar <aguilarolmomariano@gmail.com>
2025-04-09 22:27:45 +0200

    added GoogleOAuthProvider to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
   return (
-    <>
-      <Toaster/>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <>
+        <Toaster />
         <main>
           <Outlet />
         </main>
-    </>
+      </>
+    </GoogleOAuthProvider>
   );
 }
 
